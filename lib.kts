fcc4549f8f168e44dd32513c158653117f9529a5
overexerted <254196719+overexerted@users.noreply.github.com>
2026-04-19 16:58:05 -0400

    Update lib.kts

--- lib.kts
+++ lib.kts
@@ -1,3 +1,5 @@
+import dev.overexerted.substrate.api.def.ScriptModule
+
 class Library : ScriptModule {
   override fun execute(args: Map<String, Any>): Any? {
         val action = args["action"] as? String
