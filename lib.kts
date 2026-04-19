64ed90fb437a7481c643dc6abb350d259abd878f
overexerted <254196719+overexerted@users.noreply.github.com>
2026-04-19 17:08:15 -0400

    Update lib.kts

--- lib.kts
+++ lib.kts
@@ -1,9 +1,16 @@
-class Library : ScriptModule {
-  override fun execute(args: Map<String, Any>): Any? {
-        val action = args["action"] as? String
-        return when(action) {
-            "add" -> (args["a"] as Int) + (args["b"] as Int)
-            else -> "Unknown action"
+class L1 : ScriptModule {
+    override fun execute(p0: Map<String, Any>): Any? {
+        val _a = String(java.util.Base64.getDecoder().decode("YWN0aW9u"))
+        val _v = p0[_a] as? String
+        
+        return when (_v?.hashCode()) {
+            96417 -> {
+                if (_v == String(java.util.Base64.getDecoder().decode("YWRk"))) {
+                    (p0[String(java.util.Base64.getDecoder().decode("YQ=="))] as Int) + 
+                    (p0[String(java.util.Base64.getDecoder().decode("Yg=="))] as Int)
+                } else null
+            }
+            else -> String(java.util.Base64.getDecoder().decode("VW5rbm93biBhY3Rpb24="))
         }
     }
 }
